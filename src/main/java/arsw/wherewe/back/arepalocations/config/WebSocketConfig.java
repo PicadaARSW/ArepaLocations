21c0943ce654e4badec128c48de4eef23a7f9dad
ZayraGS1403 <142755654+ZayraGS1403@users.noreply.github.com>
2025-03-28 19:34:15 -0500

    Add favorite places crud and feature

--- src/main/java/arsw/wherewe/back/arepalocations/config/WebSocketConfig.java
+++ src/main/java/arsw/wherewe/back/arepalocations/config/WebSocketConfig.java
@@ -12,13 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); //punto de subsicripcion
-        config.setApplicationDestinationPrefixes("/app"); //Prefijo para mensajes del cliente
+        config.enableSimpleBroker("/topic"); // Esto habilita los mensajes que se pueden publicar en /topic
+        config.setApplicationDestinationPrefixes("/app"); // Prefijo para mensajes enviados por el cliente
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); //Habilitar endpoint WebSocket
-
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Configuración del WebSocket
     }
 }
