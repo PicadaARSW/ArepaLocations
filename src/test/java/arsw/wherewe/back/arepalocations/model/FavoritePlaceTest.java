13f3ac2d0605e7e991231c3fcf2009b0f0d02cf2
corrllr <mayerlly.suarez-c@mail.escuelaing.edu.co>
2025-04-02 16:52:51 -0500

    ADD:Documentation Swagger, env file, jacoco coverage, sonar plugin

---
+++ src/test/java/arsw/wherewe/back/arepalocations/model/FavoritePlaceTest.java
@@ -0,0 +1,52 @@
+package arsw.wherewe.back.arepalocations.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class FavoritePlaceTest {
+
+    @Test
+    void constructor_initializesTimestamp() {
+        FavoritePlace favoritePlace = new FavoritePlace();
+        assertNotNull(favoritePlace.getTimestamp());
+    }
+
+    @Test
+    void constructor_withParameters_initializesFields() {
+        FavoritePlace favoritePlace = new FavoritePlace("Central Park", 40.785091, -73.968285, "group123", 100.0f);
+        assertEquals("Central Park", favoritePlace.getPlaceName());
+        assertEquals(40.785091, favoritePlace.getLatitude());
+        assertEquals(-73.968285, favoritePlace.getLongitude());
+        assertEquals("group123", favoritePlace.getGroupId());
+        assertEquals(100.0f, favoritePlace.getRadius());
+        assertNotNull(favoritePlace.getTimestamp());
+    }
+
+    @Test
+    void setters_updateFields() {
+        FavoritePlace favoritePlace = new FavoritePlace();
+        favoritePlace.setPlaceName("Central Park");
+        favoritePlace.setLatitude(40.785091);
+        favoritePlace.setLongitude(-73.968285);
+        favoritePlace.setGroupId("group123");
+        favoritePlace.setRadius(100.0f);
+        favoritePlace.setTimestamp(1696118400000L);
+
+        assertEquals("Central Park", favoritePlace.getPlaceName());
+        assertEquals(40.785091, favoritePlace.getLatitude());
+        assertEquals(-73.968285, favoritePlace.getLongitude());
+        assertEquals("group123", favoritePlace.getGroupId());
+        assertEquals(100.0f, favoritePlace.getRadius());
+        assertEquals(1696118400000L, favoritePlace.getTimestamp());
+    }
+
+    @Test
+    void toString_returnsCorrectFormat() {
+        FavoritePlace favoritePlace = new FavoritePlace("Central Park", 40.785091, -73.968285, "group123", 100.0f);
+        favoritePlace.setId("507f1f77bcf86cd799439011");
+        String expected = "FavoritePlace{id='507f1f77bcf86cd799439011', placeName='Central Park', latitude=40.785091, longitude=-73.968285, groupId='group123', radius=100.0, timestamp=" + favoritePlace.getTimestamp() + "}";
+        assertEquals(expected, favoritePlace.toString());
+    }
+}
